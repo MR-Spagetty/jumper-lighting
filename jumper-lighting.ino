16ff36d532db6677c56e3e3c7c8e995a9be44390
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2026-04-17 21:38:29 +1200

    Begun DHD behaviour implimentation

--- jumper-lighting.ino
+++ jumper-lighting.ino
@@ -1,11 +1,24 @@
 #include <Arduino.h>
-#define COCKPIT_L_PIN D5
-#define RCOMPARTMENT_L_PIN D6
+#include <Wire.h>
+#include <IoAbstraction.h>
+#include <IoAbstractionWire.h>
+
+#define COCKPIT_L_PIN 16
+#define RCOMPARTMENT_L_PIN 0
+#define CONSOLE_LEFT_L_PIN 2
+#define CONSOLE_RIGHT_L_PIN 14
+#define ENGINE_POD_L_PIN 17
+
+MCP23017IoAbstraction DHD_EXP_1(0x20, &Wire);
+MCP23017IoAbstraction DHD_EXP_2(0x21, &Wire);
+
+const uint8_t NON_EXPANSION_DHD_PINS[] = {
+    12, 13, 15, 3, 1};
 
 #define MAX_PARAMS 5
 #define DHD_MOD_ID 0b101
 
-const uint8_t modules[] = {COCKPIT_L_PIN, RCOMPARTMENT_L_PIN};
+const uint8_t MODULES[] = {COCKPIT_L_PIN, RCOMPARTMENT_L_PIN, CONSOLE_LEFT_L_PIN, CONSOLE_RIGHT_L_PIN, ENGINE_POD_L_PIN};
 typedef union Command
 {
     struct
@@ -18,18 +31,66 @@ typedef union Command
     byte rawCommand;
 };
 
-void setup()
-{
-    pinMode(COCKPIT_L_PIN, OUTPUT);
-    pinMode(RCOMPARTMENT_L_PIN, OUTPUT);
-    digitalWrite(COCKPIT_L_PIN, LOW);
-    digitalWrite(RCOMPARTMENT_L_PIN, LOW);
-    Serial.begin(115200);
-}
-
 void handleDHD(Command command, byte param_values[])
 {
     Serial.println("Handling DHD command");
+    switch (command.nParam)
+    {
+    case 0:
+        for (uint8_t pin = 0; pin < 16; pin++)
+        {
+            byte state1 = command.state;
+            byte state2 = command.state;
+            if (command.toggle)
+            {
+                state1 = !DHD_EXP_1.digitalRead(pin);
+                state2 = !DHD_EXP_2.digitalRead(pin);
+            }
+            DHD_EXP_1.digitalWrite(pin, state1);
+            DHD_EXP_2.digitalWrite(pin, state2);
+        }
+        Serial.print(DHD_EXP_1.sync());
+        Serial.println(DHD_EXP_2.sync());
+        break;
+    case 1:
+        // TODO 1 param byte
+        Serial.println("1 byte param behaviour NYI");
+        break;
+    case 2:
+        // TODO 2 param bytes
+        Serial.println("2 byte param behavior NYI");
+        break;
+    case 5:
+        // TODO 5 param bytes
+        Serial.println("5 byte param behavior NYI");
+        break;
+    default:
+        Serial.println("Invalid number of parameters for DHD command");
+    }
+    
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin(4, 5);
+    delay(3000);
+    for (uint8_t i : MODULES)
+    {
+        pinMode(i, OUTPUT);
+        digitalWrite(i, LOW);
+    }
+    for (pinid_t pin = 0; pin < 16; pin++)
+    {
+        DHD_EXP_1.pinMode(pin, OUTPUT);
+        DHD_EXP_2.pinMode(pin, OUTPUT);
+    }
+    /* all that actually matters here is that the toggle bit
+       is 0 and the state bit is 0 and that the nParams is 0
+       as the function doesnt check anything else.
+       i have used the correct module id as good practice
+    */
+    handleDHD({.module = DHD_MOD_ID}, nullptr);
 }
 
 void loop()
@@ -53,12 +114,19 @@ void loop()
         }
     }
     Serial.println("Received command:");
-    Serial.printf("Raw command: 0x%x\nToggle: %d, State: %d, Module: %d, nParam: %d\n", command.rawCommand, command.toggle, command.state, command.module, command.nParam);
-    if (command.module >= sizeof(modules))
+    Serial.printf("Raw command: 0x%02x\nToggle: %d, "
+                  "State: %d, Module: %d, nParam: %d\n",
+                  command.rawCommand,
+                  command.toggle,
+                  command.state,
+                  command.module,
+                  command.nParam);
+    if (command.module >= sizeof(MODULES) + 1)
     {
         Serial.println("Invalid module");
         return;
     }
+    // DHD module is handled separately as it has more complex functionality
     if (command.module == DHD_MOD_ID)
     {
         handleDHD(command, params);
@@ -68,7 +136,7 @@ void loop()
     char state = command.state ? HIGH : LOW;
     if (command.toggle)
     {
-        state = !digitalRead(modules[command.module]);
+        state = !digitalRead(MODULES[command.module]);
     }
-    digitalWrite(modules[command.module], state);
+    digitalWrite(MODULES[command.module], state);
 }
